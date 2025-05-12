9823b6e32e9ac664f14f7c0be9650fc105993f7f
rahmaninov762 <160335064+rahmaninov762@users.noreply.github.com>
2025-05-12 20:59:49 +0300

    Create array1.cs

---
+++ folder/test environment/array1.cs
@@ -0,0 +1,38 @@
+// Test of arrays
+
+Console.WriteLine("Input a number from 1 to 10");
+int num = int.Parse(Console.ReadLine());
+int countWrongInput = 0;
+bool isValid = false;
+
+while (num < 1 || num > 10)
+{
+    countWrongInput++;
+    if (countWrongInput > 5)
+    {
+        Console.WriteLine("Too many wrong attempts...");
+        break;
+    }
+    Console.WriteLine("Wrong input");
+    Console.WriteLine("Input a number from 1 to 10");
+    num = int.Parse(Console.ReadLine());
+}
+
+if (!(countWrongInput > 5))
+{
+    int[] numbers = new int[num];
+    for (int a = 0; a < numbers.Length; a++)
+    {
+        numbers[a] = a + 1;
+    }
+    for (int b = 0; b < numbers.Length; b ++)
+    {
+        Console.Write($"{numbers[b]} ");
+    }
+    Console.WriteLine();
+    Console.WriteLine("EXIT");
+}
+else
+{
+    Console.WriteLine("EXIT WITH ERROR");
+}
