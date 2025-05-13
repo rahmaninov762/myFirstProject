31d02db3ad80c3235ca0a76100e16eced2906de2
rahmaninov762 <160335064+rahmaninov762@users.noreply.github.com>
2025-05-13 20:05:35 +0300

    Create 2-print-numbers-in-reverse.cs

---
+++ folder/test environment/Array/2-print-numbers-in-reverse.cs
@@ -0,0 +1,12 @@
+int n = int.Parse(Console.ReadLine());
+int[] numbers = new int[n];
+
+for (int a = 0; a < n; a ++)
+{
+    numbers[a] = int.Parse(Console.ReadLine());
+}
+
+for (int b = numbers.Length - 1; b >= 0; b --)
+{
+    Console.Write($"{numbers[b]} ");
+}
