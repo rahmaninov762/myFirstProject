37a652646da6a53118e2c4b17c1640020812b15b
rahmaninov762 <160335064+rahmaninov762@users.noreply.github.com>
2025-05-16 20:11:52 +0300

    Create 4-reverse-array-of-strings.cs

---
+++ folder/test environment/Array/4-reverse-array-of-strings.cs
@@ -0,0 +1,7 @@
+string[] inputString = Console.ReadLine().Split().ToArray();
+string[] reverseString = inputString.Reverse().ToArray();
+
+for (int a = 0; a < reverseString.Length; a ++)
+{
+    Console.Write($"{reverseString[a]} ");
+}
