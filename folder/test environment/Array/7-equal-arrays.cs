13b69124f28e4adb7e0071dd3bad71fc9cf922c8
rahmaninov762 <160335064+rahmaninov762@users.noreply.github.com>
2025-05-20 20:27:00 +0300

    Create 7-equal-arrays.cs

---
+++ folder/test environment/Array/7-equal-arrays.cs
@@ -0,0 +1,26 @@
+int[] firstArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int[] secondArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+bool areEqual = true;
+int sum = 0, element = 0;
+
+for (int i = 0; i < firstArray.Length; i++)
+{
+    if (firstArray[i] == secondArray[i])
+    {
+        sum += firstArray[i];
+    }
+    else
+    {
+        element = i;
+        areEqual = false;
+        break;
+    }
+}
+if (areEqual)
+{
+    Console.WriteLine($"Arrays are identical. Sum: {sum}");
+}
+else
+{
+    Console.WriteLine($"Arrays are not identical. Found difference at {element} index");
+}
