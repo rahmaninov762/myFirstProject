7765b88ca0360e9da6be2bf251afe4067611112f
rahmaninov762 <160335064+rahmaninov762@users.noreply.github.com>
2025-05-21 19:40:03 +0300

    Create 8-condense-array-to-number.cs

---
+++ folder/test environment/Array/8-condense-array-to-number.cs
@@ -0,0 +1,26 @@
+int[] inputArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+bool isCondensed = false;
+
+if (inputArray.Length == 1)
+{
+    Console.WriteLine(inputArray[0]);
+    isCondensed = true;
+}
+
+while (!isCondensed)
+{
+    int[] temp = new int[inputArray.Length - 1];
+    for (int a = 0; a < temp.Length; a ++)
+    {
+        temp[a] = inputArray[a] + inputArray[a + 1];
+    }
+    if (temp.Length == 1)
+    {
+        isCondensed = true;
+        Console.WriteLine(temp[0]);
+    }
+    else
+    {
+        inputArray = temp;
+    }
+}
